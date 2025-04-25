db582de369c6dac14aea855df3f90168af8fbaf6
Matteo <matteo.campana996@gmail.com>
2025-04-25 10:50:05 +0200

    Add solutions for Majority Element, Remove Duplicates from Sorted Array, and Remove Duplicates from Sorted Array II

---
+++ leetcode/80 - Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 3) return nums.length;
+        int left = 2;
+        for (int right = 2; right < nums.length; right++) {
+            if (nums[right] != nums[left - 2]) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+}
